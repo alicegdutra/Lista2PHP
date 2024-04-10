43407d1f198c3ad3d03d602e6760ae43a32ef1d6
Alice Gimenez Dutra <alicegzdutra@gmail.com>
2024-04-10 00:06:36 -0300

    ex6

---
+++ index6.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercícios</title>
+</head>
+<body>
+    <form action="exercicio6.php" method="post">
+        <?php
+            for ($i=0; $i<5; $i++){
+                echo '<input type="text" name="produtos['.$i.'][produtos]" placeholder="Nome do Produto: ">';
+                echo '<input type="number" name="preco['.$i.'][preco]" placeholder="Preço do produto: ">';
+                echo '<br>';
+            }
+        ?>
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
